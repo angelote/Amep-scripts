6475b6b2c539904024fd57d77c4fc00f414cd881
Anderson Angelote <anderson@angelote.com.br>
2026-06-03 18:29:31 -0300

    Atualização do repositório

--- Comandos_Samba.sh
+++ Comandos_Samba.sh
@@ -4,12 +4,9 @@
 # Criado em:Wed 03/Jun/2026 hs 17:43
 # ultima modificação:Wed 03/Jun/2026 hs 17:43
 # Propósito do script:
+#!/bin/bash
 
-echo "Iniciando o script de configuração do Samba..."
-
-
-MENU(){
-clear 
+MENU() {
     clear
     echo "############################################"
     echo "#           MENU CONEXÃO                   #"
@@ -18,36 +15,42 @@ clear
     echo "# 01 - Listar Usuários                     #"
     echo "# 02 - Adicionar Usuário                   #"
     echo "#                                          #"
-    echo "#                                          #"
-    echo "#                                          #"
-    echo "#                                          #"
-    echo "#                                          #"
     echo "# 99 - Sair                                #"
     echo "#                                          #"
     echo "############################################"
 
     echo -n "Digite a opção desejada: "
     read OPC
-    case $OPC in
-    1 | 01)
-        echo "Listando usuários do Samba..."
-         samba-tool user list
-         read -p "Pressione [Enter] para continuar..."
 
-        ;;
+    case "$OPC" in
+        1 | 01)
+            echo "Listando usuários do Samba..."
+            samba-tool user list
+            read -p "Pressione [Enter] para continuar..."
+            return 0
+            ;;
 
-    99) 
-        break 
-        ;;
-   esac
+        2 | 02)
+            echo "Função de adicionar usuário ainda não implementada."
+            read -p "Pressione [Enter] para continuar..."
+            return 0
+            ;;
+
+        99)
+            # Sinaliza para o while parar
+            return 1
+            ;;
+
+        *)
+            echo "Opção inválida!"
+            read -p "Pressione [Enter] para continuar..."
+            return 0
+            ;;
+    esac
 }
 
-
-while true
-do
+while true; do
     MENU || break
 done
 
-  
-
-
+echo "Saindo do menu..."
