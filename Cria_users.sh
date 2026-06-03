ecab84c52c047b89255561e626e215ae4e2156c4
Anderson Angelote <anderson@angelote.com.br>
2026-06-03 17:46:04 -0300

    Atualização do repositório

---
+++ Cria_users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 
+# Autor: Anderson Angelote
+# Criado em:Wed 03/Jun/2026 hs 17:43
+# ultima modificação:Wed 03/Jun/2026 hs 17:43
+# Propósito do script:
+
---
+++ envia_git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 
+# Autor: Anderson Angelote
+# Criado em:Wed 03/Jun/2026 hs 17:44
+# ultima modificação:Wed 03/Jun/2026 hs 17:44
+# Propósito do script:
+
+git add -A
+git commit -m "Atualização do repositório "
+git push --set-upstream origin HEAD
+
